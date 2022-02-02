cd804d2b137e88afceded6d3ae98c68963a39700
Your Name <you@example.com>
2022-02-02 14:46:41 +0900

    Changed the HTML for the site.

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from .models import Post, Servey, Tag
 
+class ServeyAdmin(admin.ModelAdmin):
+	list_display = ('student_number','needs','question','institution','published_date')
+	ordering = ('-institution',)
+	list_filter = ('needs',)
+	search_fields = ('student_number', 'question', 'institution')
+
 admin.site.register(Post)
-admin.site.register(Servey)
+admin.site.register(Servey, ServeyAdmin)
 admin.site.register(Tag)
