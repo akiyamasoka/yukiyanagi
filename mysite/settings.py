f17ad7e7e9811ce94554b069bfa3acc542eab3a7
Your Name <you@example.com>
2022-02-09 23:33:06 +0900

    Changed the HTML for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -121,8 +121,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
 
-STATIC_URL = '/media/'
-STATIC_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
